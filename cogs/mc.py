836f4e0d01f673e09aa66e3cfb178c11234600e1
YasInvolved <jbaczyk27@gmail.com>
2026-07-16 04:36:26 +0200

    modify embed

--- cogs/mc.py
+++ cogs/mc.py
@@ -9,18 +9,21 @@ class Minecraft(CustomCog):
         super().__init__(bot)
 
     @app_commands.command(name="teren", description="Zarejestruj swój teren")
-    async def register_plot(self, interaction: discord.Interaction, nazwa: str, x: int, y: int):
+    async def register_plot(self, interaction: discord.Interaction, nazwa: str, x: int, y: int, rozmiar_x: int, rozmiar_y: int):
         embed = discord.Embed(
             title="ℹ️ Nowy teren!",
             color=discord.Color.green()
         )
 
-        embed.add_field(name="Nazwa", value=nazwa, inline=False)
-        embed.add_field(name="Koordynaty", value=f"{x}, {y}", inline=False)
-        embed.add_field(name="Właściciel", value=f"{interaction.user.mention}", inline=False)
-        embed.set_image(url="attachment://jasper-wow.gif")
-
         try:
+            embed.add_field(name="X", value=str(x), inline=True)
+            embed.add_field(name="Y", value=str(y), inline=True)
+            embed.add_field(name="Nazwa", value=nazwa, inline=False)
+            embed.add_field(name="Rozmiar X", value=str(rozmiar_x), inline=True)
+            embed.add_field(name="Rozmiar Y", value=str(rozmiar_y), inline=True)
+            embed.add_field(name="Właściciel", value=f"{interaction.user.mention}", inline=False)
+            embed.set_image(url="attachment://jasper-wow.gif")
+
             gif = discord.File(self.get_content_filename("jasper-iphone-wow.gif"), filename="jasper-wow.gif")
             await self.plots_channel.send(file=gif, embed=embed)
             await interaction.response.send_message("Działka zarejestrowana!", ephemeral=True)
